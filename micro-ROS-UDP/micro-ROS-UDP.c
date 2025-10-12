533818a7faba4e6f02df09031640d2b8b46fd182
Werllley007 <thiagowerlley@ufc.br>
2025-10-12 14:13:42 -0300

    Atualizar

--- micro-ROS-UDP/micro-ROS-UDP.c
+++ micro-ROS-UDP/micro-ROS-UDP.c
@@ -86,7 +86,7 @@ int main()
     cyw43_arch_enable_sta_mode();
 
     printf("Connecting to WiFi...\n");
-    if (cyw43_arch_wifi_connect_timeout_ms("brisa-4475287", "cboiegzs", CYW43_AUTH_WPA2_AES_PSK, 30000)) {
+    if (cyw43_arch_wifi_connect_timeout_ms(WIFI_SSID, WIFI_PASSWORD, CYW43_AUTH_WPA2_AES_PSK, 30000)) {
         printf("failed to connect.\n");
         return 1;
     } else {
